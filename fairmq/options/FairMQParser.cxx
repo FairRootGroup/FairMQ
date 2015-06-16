6cd1e53b13b5fbf59e58b061ec9d35197af345a4
Mohammad Al-Turany <m.al-turany@gsi.de>
2015-06-16 15:31:23 +0200

    Add missing license information

--- fairmq/options/FairMQParser.cxx
+++ fairmq/options/FairMQParser.cxx
@@ -1,4 +1,11 @@
-/* 
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+/*
  * File:   FairMQParser.cxx
  * Author: winckler
  * 
--- fairmq/options/FairProgOptions.h
+++ fairmq/options/FairProgOptions.h
@@ -1,4 +1,12 @@
-/* 
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+/*
  * File:   FairProgOptions.h
  * Author: winckler
  *
