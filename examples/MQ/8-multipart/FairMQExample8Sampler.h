dcb791694ab2d7d688941df629404a5e5de69ba7
winckler <n.winckler@gsi.de>
2016-03-28 19:13:49 +0200

    change serialization API so that it takes a FairMQMessage reference

--- examples/MQ/8-multipart/FairMQExample8Sampler.h
+++ examples/MQ/8-multipart/FairMQExample8Sampler.h
@@ -19,48 +19,6 @@
 
 #include "FairMQDevice.h"
 
-
-struct MyPoint
-{
-        /* data */
-    double x=6;
-    double y=6;
-    double z=6;
-};
-
-struct Ex8Header {
-  int32_t stopFlag;
-};
-
-
-
-struct MyPointSerializer
-{
-    void Serialize(std::unique_ptr<FairMQMessage>& msg, MyPoint* input)
-    {
-        int DataSize = sizeof(MyPoint);
-        msg->Rebuild(DataSize);
-        MyPoint* digiptr = reinterpret_cast<MyPoint*>(msg->GetData());
-        digiptr->x=input->x;
-        digiptr->y=input->y;
-        digiptr->z=input->z;
-    }
-
-    void Deserialize(std::unique_ptr<FairMQMessage>& msg, MyPoint* input)
-    {
-        MyPoint* digiptr = static_cast<MyPoint*>(msg->GetData());
-        input->x=digiptr->x;
-        input->y=digiptr->y;
-        input->z=digiptr->z;
-    }
-
-};
-
-
-
-
-
-
 class FairMQExample8Sampler : public FairMQDevice
 {
   public:
