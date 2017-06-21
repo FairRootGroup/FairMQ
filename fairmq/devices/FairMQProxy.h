92cae6f9a4f69786115343e4d055b77759e56c90
Alexey Rybalchenko <alexryba@gmail.com>
2017-06-21 12:17:01 +0200

    FairMQProxy: remove outdated code

--- fairmq/devices/FairMQProxy.h
+++ fairmq/devices/FairMQProxy.h
@@ -22,22 +22,8 @@
 class FairMQProxy : public FairMQDevice
 {
   public:
-    enum
-    {
-        Multipart = FairMQDevice::Last,
-        Last
-    };
-
     FairMQProxy();
     virtual ~FairMQProxy();
-
-    virtual void SetProperty(const int key, const std::string& value);
-    virtual std::string GetProperty(const int key, const std::string& default_ = "");
-    virtual void SetProperty(const int key, const int value);
-    virtual int GetProperty(const int key, const int default_ = 0);
-
-    virtual std::string GetPropertyDescription(const int key);
-    virtual void ListProperties();
 
   protected:
     int fMultipart;
