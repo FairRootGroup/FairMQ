724b527d7014bd526b5ef539480836af70038381
Dennis Klein <d.klein@gsi.de>
2018-05-02 11:55:46 +0200

    Add CI jobs
    Resolves #12

---
+++ FairMQTest.cmake
@@ -0,0 +1,73 @@
+################################################################################
+#    Copyright (C) 2018 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+Set(CTEST_SOURCE_DIRECTORY $ENV{SOURCEDIR})
+Set(CTEST_BINARY_DIRECTORY $ENV{BUILDDIR})
+Set(CTEST_SITE $ENV{SITE})
+Set(CTEST_BUILD_NAME $ENV{LABEL})
+Set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+Set(CTEST_PROJECT_NAME "FairMQ")
+
+Find_Program(CTEST_GIT_COMMAND NAMES git)
+Set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
+
+Set(BUILD_COMMAND "make")
+Set(CTEST_BUILD_COMMAND "${BUILD_COMMAND} -j$ENV{number_of_processors}")
+
+String(TOUPPER $ENV{ctest_model} _Model)
+Set(configure_options "-DCMAKE_BUILD_TYPE=${_Model}")
+
+Set(CTEST_USE_LAUNCHERS 1)
+Set(configure_options "${configure_options};-DCTEST_USE_LAUNCHERS=${CTEST_USE_LAUNCHERS}")
+
+Set(configure_options "${configure_options};-DDISABLE_COLOR=ON")
+
+Set(EXTRA_FLAGS $ENV{EXTRA_FLAGS})
+If(EXTRA_FLAGS)
+  Set(configure_options "${configure_options};${EXTRA_FLAGS}") 
+EndIf()
+
+If($ENV{ctest_model} MATCHES Nightly OR $ENV{ctest_model} MATCHES Profile)
+
+  Find_Program(GCOV_COMMAND gcov)
+  If(GCOV_COMMAND)
+    Message("Found GCOV: ${GCOV_COMMAND}")
+    Set(CTEST_COVERAGE_COMMAND ${GCOV_COMMAND})
+  EndIf(GCOV_COMMAND)
+
+  Set(ENV{ctest_model} Nightly)
+
+  CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+
+EndIf()
+
+Ctest_Start($ENV{ctest_model})
+
+If(NOT $ENV{ctest_model} MATCHES Experimental)
+  Ctest_Update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+EndIf()
+
+Ctest_Configure(BUILD "${CTEST_BINARY_DIRECTORY}"
+                OPTIONS "${configure_options}"
+               )
+
+Ctest_Build(BUILD "${CTEST_BINARY_DIRECTORY}")
+
+Ctest_Test(BUILD "${CTEST_BINARY_DIRECTORY}" 
+           PARALLEL_LEVEL $ENV{number_of_processors}
+           RETURN_VALUE _ctest_test_ret_val
+          )
+
+If(GCOV_COMMAND)
+  Ctest_Coverage(BUILD "${CTEST_BINARY_DIRECTORY}")
+EndIf()
+
+Ctest_Submit()
+ 
+if (_ctest_test_ret_val)
+  Message(FATAL_ERROR "Some tests failed.")
+endif()
---
+++ Jenkinsfile
@@ -0,0 +1,56 @@
+#!groovy
+
+def specToLabel(Map spec) {
+  return "${spec.os}-${spec.arch}-${spec.compiler}-FairSoft_${spec.fairsoft}"
+}
+
+def buildMatrix(List specs, Closure callback) {
+  def nodes = [:]
+  for (spec in specs) {
+    def label = specToLabel(spec)
+    nodes[label] = {
+      node(label) {
+        githubNotify(context: "alfa-ci/${label}", description: 'Building ...', status: 'PENDING')
+        try {
+          deleteDir()
+          checkout scm
+
+          callback.call(spec, label)
+
+          deleteDir()
+          githubNotify(context: "alfa-ci/${label}", description: 'Success', status: 'SUCCESS')
+        } catch (e) {
+          deleteDir()
+          githubNotify(context: "alfa-ci/${label}", description: 'Error', status: 'ERROR')
+          throw e
+        }
+      }
+    }
+  }
+  return nodes
+}
+
+pipeline{
+  agent none
+  stages {
+    stage("Run Build/Test Matrix") {
+      steps{
+        script {
+          parallel(buildMatrix([
+            [os: 'Debian8',    arch: 'x86_64', compiler: 'gcc4.9',         fairsoft: 'apr18'],
+            [os: 'MacOS10.11', arch: 'x86_64', compiler: 'AppleLLVM8.0.0', fairsoft: 'apr18'],
+            // [os: 'MacOS10.13', arch: 'x86_64', compiler: 'AppleLLVM9.0.0', fairsoft: 'oct17'],
+          ]) { spec, label ->
+            sh '''\
+              echo "export BUILDDIR=$PWD/build" >> Dart.cfg
+              echo "export SOURCEDIR=$PWD" >> Dart.cfg
+              echo "export PATH=$SIMPATH/bin:$PATH" >> Dart.cfg
+              echo "export GIT_BRANCH=$JOB_BASE_NAME" >> Dart.cfg
+            '''
+            sh './Dart.sh alfa_ci Dart.cfg'
+          })
+        }
+      }
+    }
+  }
+}
