e95096eb37d862e51c444d97e4aaf781346429b9
Matthias Kretz <kretz@kde.org>
2018-08-20 16:08:04 +0200

    Add RateLimiter class to tools

--- fairmq/Tools.h
+++ fairmq/Tools.h
@@ -13,6 +13,7 @@
 #include <fairmq/tools/CppSTL.h>
 #include <fairmq/tools/Network.h>
 #include <fairmq/tools/Process.h>
+#include <fairmq/tools/RateLimit.h>
 #include <fairmq/tools/Strings.h>
 #include <fairmq/tools/Unique.h>
 #include <fairmq/tools/Version.h>
