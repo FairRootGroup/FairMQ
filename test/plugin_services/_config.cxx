36f409dc72c71e20556179dbefe1f3ed129aa5b9
Alexey Rybalchenko <alexryba@gmail.com>
2019-02-06 16:37:47 +0100

    Formatting

--- test/plugin_services/_config.cxx
+++ test/plugin_services/_config.cxx
@@ -22,15 +22,15 @@ TEST_F(PluginServices, ConfigSynchronous)
 {
     mServices.SubscribeToDeviceStateChange("test",[&](DeviceState newState){
         switch (newState) {
-        case DeviceState::InitializingDevice:
-            mServices.SetProperty<int>("blubb", 42);
-            break;
-        case DeviceState::DeviceReady:
-            EXPECT_EQ(mServices.GetProperty<int>("blubb"), 42);
-            EXPECT_EQ(mServices.GetPropertyAsString("blubb"), fair::mq::tools::ToString(42));
-            break;
-        default:
-            break;
+            case DeviceState::InitializingDevice:
+                mServices.SetProperty<int>("blubb", 42);
+                break;
+            case DeviceState::DeviceReady:
+                EXPECT_EQ(mServices.GetProperty<int>("blubb"), 42);
+                EXPECT_EQ(mServices.GetPropertyAsString("blubb"), fair::mq::tools::ToString(42));
+                break;
+            default:
+                break;
         }
     });
 }
@@ -39,11 +39,11 @@ TEST_F(PluginServices, ConfigInvalidStateError)
 {
     mServices.SubscribeToDeviceStateChange("test",[&](DeviceState newState){
         switch (newState) {
-        case DeviceState::DeviceReady:
-            ASSERT_THROW(mServices.SetProperty<int>("blubb", 42), fair::mq::PluginServices::InvalidStateError);
-            break;
-        default:
-            break;
+            case DeviceState::DeviceReady:
+                ASSERT_THROW(mServices.SetProperty<int>("blubb", 42), fair::mq::PluginServices::InvalidStateError);
+                break;
+            default:
+                break;
         }
     });
 }
