3da5f4d5db9d006d6e3be743de2d810f8bc85e4c
Dennis Klein <d.klein@gsi.de>
2019-07-01 17:13:11 +0200

    SDK: Add CMake skeleton
    * New build flag BUILD_SDK
    * New component "sdk"

---
+++ fairmq/sdk/runFairMQ.cxx
@@ -0,0 +1,15 @@
+/********************************************************************************
+ *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+#include <fairmq/sdk/Topology.h>
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
