5168280626eaec7e039e686dad75ac45f620674a
Dennis Klein <d.klein@gsi.de>
2019-07-17 19:41:53 +0200

    Example.DDS: Fix support for ssh rms plugin

---
+++ examples/dds/fairmq-ex-dds-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+################################################################################
+#    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+
+export PATH=@BIN_DIR@:$PATH
