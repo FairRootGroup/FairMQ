0eacec9850a844f557863d5ee27369c1da9dd26a
Alexey Rybalchenko <alexryba@gmail.com>
2019-07-24 14:16:35 +0200

    SDK: Add test for timeout, concurrent call. Implement TODOs

--- test/sdk/_topology.cxx
+++ test/sdk/_topology.cxx
@@ -21,26 +21,24 @@ TEST_F(Topology, Construction)
     fair::mq::sdk::Topology topo(mDDSTopo, mDDSSession);
 }
 
-TEST_F(Topology, ChangeState_async1)
+TEST_F(Topology, ChangeStateAsync)
 {
     using fair::mq::sdk::Topology;
     using fair::mq::sdk::TopologyTransition;
 
     Topology topo(mDDSTopo, mDDSSession);
     fair::mq::tools::Semaphore blocker;
-    topo.ChangeState(TopologyTransition::Stop, [&blocker](Topology::ChangeStateResult result) {
+    topo.ChangeState(TopologyTransition::Stop, [&blocker, &topo](Topology::ChangeStateResult result) {
         LOG(info) << result;
         EXPECT_EQ(result.rc, fair::mq::AsyncOpResultCode::Ok);
-        // TODO add the helper to check state consistency
-        for (const auto& e : result.state) {
-            EXPECT_EQ(e.second.state, fair::mq::sdk::DeviceState::Ready);
-        }
+        EXPECT_NO_THROW(fair::mq::sdk::AggregateState(result.state));
+        EXPECT_EQ(fair::mq::sdk::StateEqualsTo(result.state, fair::mq::sdk::DeviceState::Ready), true);
         blocker.Signal();
     });
     blocker.Wait();
 }
 
-TEST_F(Topology, ChangeState_sync)
+TEST_F(Topology, ChangeStateSync)
 {
     using fair::mq::sdk::Topology;
     using fair::mq::sdk::TopologyTransition;
@@ -49,27 +47,41 @@ TEST_F(Topology, ChangeState_sync)
     auto result(topo.ChangeState(TopologyTransition::Stop));
 
     EXPECT_EQ(result.rc, fair::mq::AsyncOpResultCode::Ok);
-    // TODO add the helper to check state consistency
-    for (const auto& e : result.state) {
-        EXPECT_EQ(e.second.state, fair::mq::sdk::DeviceState::Ready);
-    }
+    EXPECT_NO_THROW(fair::mq::sdk::AggregateState(result.state));
+    EXPECT_EQ(fair::mq::sdk::StateEqualsTo(result.state, fair::mq::sdk::DeviceState::Ready), true);
 }
-// TEST_F(Topology, Timeout)
-// {
-//     using fair::mq::sdk::Topology;
-//     using fair::mq::sdk::TopologyTransition;
 
-//     Topology topo(mDDSTopo, mDDSSession);
-//     Topology::ChangeStateResult r;
-//     fair::mq::tools::Semaphore blocker;
-//     topo.ChangeState(TopologyTransition::End, [&](Topology::ChangeStateResult result) {
-//         LOG(info) << result;
-//         blocker.Signal();
-//     }, std::chrono::milliseconds(100));
-//     blocker.Wait();
-//     for (const auto& e : r.rc) {
-//         EXPECT_EQ(e.second.state, fair::mq::sdk::DeviceState::Ok);
-//     }
-// }
+TEST_F(Topology, ChangeStateConcurrent)
+{
+    using fair::mq::sdk::Topology;
+    using fair::mq::sdk::TopologyTransition;
+
+    Topology topo(mDDSTopo, mDDSSession);
+    fair::mq::tools::Semaphore blocker;
+    topo.ChangeState(TopologyTransition::Stop, [&blocker](Topology::ChangeStateResult result) {
+        LOG(info) << "result for valid ChangeState: " << result;
+        blocker.Signal();
+    });
+    topo.ChangeState(TopologyTransition::Stop, [&blocker](Topology::ChangeStateResult result) {
+        LOG(info) << "result for invalid ChangeState: " << result;
+        EXPECT_EQ(result.rc, fair::mq::AsyncOpResultCode::Error);
+    });
+    blocker.Wait();
+}
+
+TEST_F(Topology, ChangeStateTimeout)
+{
+    using fair::mq::sdk::Topology;
+    using fair::mq::sdk::TopologyTransition;
+
+    Topology topo(mDDSTopo, mDDSSession);
+    fair::mq::tools::Semaphore blocker;
+    topo.ChangeState(TopologyTransition::End, [&](Topology::ChangeStateResult result) {
+        LOG(info) << result;
+        EXPECT_EQ(result.rc, fair::mq::AsyncOpResultCode::Timeout);
+        blocker.Signal();
+    }, std::chrono::milliseconds(1));
+    blocker.Wait();
+}
 
 }   // namespace
