caeee626a3b1c020da91aac1e0cb490db765cb78
Dennis Klein <d.klein@gsi.de>
2019-10-07 12:31:22 +0200

    PMIx plugin: Add barriers and fix lookup of multiple keys

--- fairmq/plugins/PMIx/PMIxPlugin.cxx
+++ fairmq/plugins/PMIx/PMIxPlugin.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2017 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -8,7 +8,6 @@
 
 #include "PMIxPlugin.h"
 
-#include <boost/algorithm/string/join.hpp>
 #include <fairmq/Tools.h>
 #include <stdexcept>
 
@@ -27,14 +26,28 @@ PMIxPlugin::PMIxPlugin(const std::string& name,
     : Plugin(name, version, maintainer, homepage, pluginServices)
     , fPid(getpid())
 {
+    Init();
+    SetProperty<std::string>("id", std::string(fProc.nspace) + "_" + std::to_string(fProc.rank));
+    Fence();
+
     SubscribeToDeviceStateChange([&](DeviceState newState) {
         switch (newState) {
-            case DeviceState::Connecting:
-                Init();
-                Publish();
-                Fence();
-                Lookup();
-                break;
+        case DeviceState::Idle:
+            Fence();
+            break;
+        case DeviceState::Bound:
+            Publish();
+            Fence();
+            break;
+        case DeviceState::Connecting:
+            Lookup();
+            break;
+        case DeviceState::DeviceReady:
+            Fence();
+            break;
+        case DeviceState::Ready:
+            Fence();
+            break;
         case DeviceState::Exiting:
             UnsubscribeFromDeviceStateChange();
             break;
@@ -49,9 +62,9 @@ PMIxPlugin::~PMIxPlugin()
     while (pmix::initialized()) {
         try {
             pmix::finalize();
-            LOG(debug) << PMIxClient() << " pmix::finalize() OK";
+            LOG(debug) << PMIxClient() << "pmix::finalize() OK";
         } catch (const pmix::runtime_error& e) {
-            LOG(debug) << PMIxClient() << " pmix::finalize() failed: " << e.what();
+            LOG(debug) << PMIxClient() << "pmix::finalize() failed: " << e.what();
         }
     }
 }
@@ -59,7 +72,7 @@ PMIxPlugin::~PMIxPlugin()
 auto PMIxPlugin::PMIxClient() const -> std::string
 {
     std::stringstream ss;
-    ss << "PMIx client(pid=" << fPid << ")";
+    ss << "PMIx client(pid=" << fPid << ") ";
     return ss.str();
 }
 
@@ -67,7 +80,7 @@ auto PMIxPlugin::Init() -> void
 {
     if (!pmix::initialized()) {
         fProc = pmix::init();
-        LOG(debug) << PMIxClient() << " pmix::init() OK: " << fProc
+        LOG(debug) << PMIxClient() << "pmix::init() OK: " << fProc
                    << ",version=" << pmix::get_version();
     }
 }
@@ -90,7 +103,7 @@ auto PMIxPlugin::Publish() -> void
 
     if (info.size() > 0) {
         pmix::publish(info);
-        LOG(debug) << PMIxClient() << " pmix::publish() OK: published "
+        LOG(debug) << PMIxClient() << "pmix::publish() OK: published "
                    << info.size() << " binding channels.";
     }
 }
@@ -101,44 +114,43 @@ auto PMIxPlugin::Fence() -> void
     all.rank = pmix::rank::wildcard;
 
     pmix::fence({all});
+    LOG(debug) << PMIxClient() << "pmix::fence() OK";
 }
 
 auto PMIxPlugin::Lookup() -> void
 {
     auto channels(GetChannelInfo());
-    std::vector<pmix::pdata> pdata;
-
     for (const auto& c : channels) {
         std::string methodKey{"chans." + c.first + "." + std::to_string(c.second - 1) + ".method"};
         if (GetProperty<std::string>(methodKey) == "connect") {
             for (int i = 0; i < c.second; ++i) {
+                std::vector<pmix::pdata> pdata;
                 std::string addressKey{"chans." + c.first + "." + std::to_string(i) + ".address"};
                 pdata.emplace_back();
                 pdata.back().set_key(addressKey);
+                std::vector<pmix::info> info;
+                info.emplace_back(PMIX_WAIT, static_cast<int>(pdata.size()));
+
+                if (pdata.size() > 0) {
+                    pmix::lookup(pdata, info);
+                    LOG(debug) << PMIxClient() << "pmix::lookup() OK";
+                }
+
+                for (const auto& p : pdata) {
+                    if (p.value.type == PMIX_UNDEF) {
+                        LOG(debug) << PMIxClient() << "pmix::lookup() not found: key=" << p.key;
+                    } else if (p.value.type == PMIX_STRING) {
+                        LOG(debug) << PMIxClient() << "pmix::lookup() found:"
+				   << " key=" << p.key << ",value=" << p.value.data.string;
+                        SetProperty<std::string>(p.key, p.value.data.string);
+                    } else {
+                        LOG(debug) << PMIxClient() << "pmix::lookup() wrong type returned: "
+				   << "key=" << p.key << ",type=" << p.value.type;
+                    }
+                }
             }
         }
     }
-
-    if (pdata.size() > 0) {
-        pmix::lookup(pdata);
-        LOG(debug) << PMIxClient() << " pmix::lookup() OK";
-    }
-
-    LOG(info) << pdata.size();
-
-    for (const auto& p : pdata) {
-        if (p.value.type == PMIX_UNDEF) {
-            LOG(debug) << PMIxClient() << " pmix::lookup() not found: key=" << p.key;
-        } else if (p.value.type == PMIX_STRING) {
-            LOG(debug) << PMIxClient() << " pmix::lookup() found: key=" << p.key << ",value=" << p.value.data.string;
-            SetProperty<std::string>(p.key, p.value.data.string);
-            LOG(info) << GetProperty<std::string>(p.key);
-        } else {
-            LOG(debug) << PMIxClient() << " pmix::lookup() wrong type returned: key=" << p.key << ",type=" << p.value.type;
-        }
-    }
-
-    LOG(info) << pdata.size();
 }
 
 } /* namespace plugins */
