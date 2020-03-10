34494e7f5418fe990c58483ba670c370b1861d9d
Dennis Klein <d.klein@gsi.de>
2020-03-10 14:06:41 +0100

    Silence -Wunused-parameter

--- examples/qc/runSink.cxx
+++ examples/qc/runSink.cxx
@@ -21,5 +21,5 @@ class Sink : public FairMQDevice
 };
 
 namespace bpo = boost::program_options;
-void addCustomOptions(bpo::options_description& options) {}
+void addCustomOptions(bpo::options_description&) {}
 FairMQDevicePtr getDevice(const fair::mq::ProgOptions& /*config*/) { return new Sink(); }
