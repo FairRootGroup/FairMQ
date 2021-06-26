07d2d3e00b386105ed76fa29233614c3a267a9e2
Dennis Klein <d.klein@gsi.de>
2021-06-26 02:58:59 +0200

    build: Have color output depend on a common switch DISABLE_COLOR

--- cmake/FairMQProjectSettings.cmake
+++ cmake/FairMQProjectSettings.cmake
@@ -119,11 +119,11 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g ${_warnings} -DNDEBUG ${_sanitizers}"
 unset(_warnings)
 unset(_sanitizers)
 
-if(CMAKE_GENERATOR STREQUAL "Ninja" AND
-   ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9) OR
-    (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)))
-  # Force colored warnings in Ninja's output, if the compiler has -fdiagnostics-color support.
-  # Rationale in https://github.com/ninja-build/ninja/issues/814
+if(DISABLE_COLOR)
+  set(CMAKE_COLOR_MAKEFILE OFF)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=never")
+else()
+  set(CMAKE_COLOR_MAKEFILE ON)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
 endif()
 
