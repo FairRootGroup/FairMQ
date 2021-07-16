479d6e071234fdb2541fb7d666eab99fbff58120
Dennis Klein <d.klein@gsi.de>
2021-07-16 18:06:52 +0200

    feat: Add <fairmq/FwdDecls.h>

---
+++ fairmq/FwdDecls.h
@@ -0,0 +1,31 @@
+/********************************************************************************
+ *    Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+#ifndef FAIR_MQ_FWDDECLS_H
+#define FAIR_MQ_FWDDECLS_H
+
+#include <fairmq/ProgOptionsFwd.h>
+
+class FairMQChannel;
+class FairMQDevice;
+class FairMQMemoryResource;
+class FairMQMessage;
+class FairMQParts;
+class FairMQPoller;
+class FairMQRegionBlock;
+class FairMQRegionConfig;
+class FairMQRegionInfo;
+class FairMQSocket;
+class FairMQTransportFactory;
+class FairMQUnmanagedRegion;
+
+namespace fair::mq {
+class FairMQMemoryResource;
+}
+
+#endif   // FAIR_MQ_FWDDECLS_H
