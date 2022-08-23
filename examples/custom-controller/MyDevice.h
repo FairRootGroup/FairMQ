48448817c4c507acb8b8948cfb18faee25b0032c
Dennis Klein <d.klein@gsi.de>
2022-08-23 20:10:20 +0200

    feat(examples): Add new example with custom controller plugin (statically compiled in)

---
+++ examples/custom-controller/MyDevice.h
@@ -0,0 +1,21 @@
+/********************************************************************************
+ *    Copyright (C) 2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+#ifndef FAIR_MQ_EXAMPLE_CUSTOM_CONTROLLER_DEVICE
+#define FAIR_MQ_EXAMPLE_CUSTOM_CONTROLLER_DEVICE
+
+#include <fairmq/Device.h>
+
+namespace example {
+
+using MyDevice = typename fair::mq::Device;
+
+}   // namespace example
+
+#endif /* FAIR_MQ_EXAMPLE_CUSTOM_CONTROLLER_DEVICE */
+
